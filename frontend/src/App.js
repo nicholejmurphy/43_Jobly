ada0b898065e1e76cb3f76b888e07ea3eb34ef56
Nichole Murphy <nicholejmurphy@gmail.com>
2023-09-23 13:00:55 -0400

    Updated App.js to contain state and api functions to pass through UserContext.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,115 @@
-import React from "react";
-import "./static/App.css";
+import React, { useState, useState } from "react";
+import "./App.css";
 import { useLocalStorage } from "./hooks";
-import NavBar from "./NavBar";
-import Routes from "./Routes";
-import UserContext from "./UserContext";
+import jwt from "jsonwebtoken";
+import JoblyApi from "./api/api";
+import UserContext from "./auth/UserContext";
+import Loading from "./common/Loading";
+import Navigation from "./navigation_routes/Navigation";
 
+/** Jobly Application
+ *
+ * - dataIsLoading: Identifies if user data has been retrieved.
+ *   Manages loading component to show or not.
+ *
+ * - token: Saved in localStorage and stores username in payload.
+ *
+ * - currUser: holds user data for currently logged in user.
+ *
+ * - applicationIds: stores set of user's applications.
+ *   Used when identifying if job has already been applied to
+ *   in JobCard component.
+ */
 function App() {
-  // Runs GET request for items from JSON-server with first render
-  const user = useLocalStorage("user");
+  const [dataIsLoading, setDataIsLoading] = useState(true);
+  const [token, setToken] = useLocalStorage("user_token");
+  const [currUser, setCurrUser] = useState(null);
+  const [applicationIds, setApplicationIds] = useState(new Set([]));
+
+  /** Updates if token changes.
+   *  - dataIsLoading => { true } while awaiting data
+   *  - Retrieves user data from token and pulls from JoblyApi
+   *  - Updates currUser & applicationIds
+   *  - dataIsLoading => { false } after useEffect
+   */
+  useEffect(
+    function loadUserInfo() {
+      async function getCurrUser() {
+        if (token) {
+          try {
+            const { username } = jwt.decode(token);
+            // Set api token
+            JoblyApi.token = token;
+            const user = await JoblyApi.getCurrUser(username);
+            setCurrUser(user);
+            setApplicationIds(new Set(currUser.applications));
+          } catch (error) {
+            console.error("Failed to set current user.", error);
+            setCurrUser(null);
+          }
+        }
+        setDataIsLoading(false);
+      }
+      setDataIsLoading(true);
+      getCurrUser();
+    },
+    [token]
+  );
+
+  // Check if user has applied to job.
+  // - Is jobId in applicationsId?
+  function hasApplied(id) {
+    return applicationIds.has(id);
+  }
+
+  // User applies to job
+  // - Send api request to apply to job
+  // - Add jobId to applicationIds set
+  async function applyToJob(id) {
+    if (hasApplied(id)) return;
+    JoblyApi.apply(currUser.username, id);
+    setApplicationIds(new Set([...applicationIds, id]));
+  }
+
+  // User login { username, password } => { token }
+  async function login(data) {
+    try {
+      const token = await JoblyApi.login(data);
+      setToken(token);
+      return { success: true };
+    } catch (error) {
+      console.error("Error with login. Invalid username or password.", error);
+      return { success: false };
+    }
+  }
+
+  // User signup { user data } => { token }
+  async function signup(data) {
+    try {
+      const token = await JoblyApi.signup(data);
+      setToken(token);
+      return { success: true };
+    } catch (error) {
+      console.error("Error with signup.", error);
+      return { success: false };
+    }
+  }
+
+  // User logout.
+  async function logout() {
+    setCurrUser(null);
+    setToken(null);
+  }
+
+  // Soow loading component if data is still loading.
+  if (dataIsLoading) {
+    return <Loading />;
+  }
 
   return (
-    <UserContext.Provider value={user}>
-      <NavBar />
-      <Routes />
+    <UserContext.Provider value={{ currUser, hasApplied, applyToJob }}>
+      <Navigation logout={logout} />
+      <Routes login={login} signup={signup} />
     </UserContext.Provider>
   );
 }
