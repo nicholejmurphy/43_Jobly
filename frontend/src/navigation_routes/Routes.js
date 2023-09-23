1878baf573d9c0908c8e764512d2ba026a53eb83
Nichole Murphy <nicholejmurphy@gmail.com>
2023-09-23 14:21:57 -0400

    Added Routes component and PrivateRoute for auth nav

--- frontend/src/navigation_routes/Routes.js
+++ frontend/src/navigation_routes/Routes.js
@@ -1,5 +1,44 @@
 import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import Homepage from "../homepage/Homepage";
+import CompanyList from "../companies/CompanyList";
+import CompanyDetails from "../companies/CompanyDetails";
+import JobList from "../jobs/JobList";
+import ProfileForm from "../profiles/ProfileForm";
+import LoginForm from "../auth/LoginForm";
+import SignupForm from "../auth/SignupForm";
+import NotFound from "../common/NotFound";
 
-function Routes() {}
+function Routes({ login, signup }) {
+  return (
+    <div>
+      <Route exact path="/">
+        <Homepage />
+      </Route>
+      <Route path="/login">
+        <LoginForm login={login} />
+      </Route>
+      <Route path="/signup">
+        <SignupForm signup={signup} />
+      </Route>
+      <PrivateRoute exact path="/companies">
+        <CompanyList />
+      </PrivateRoute>
+      <PrivateRoute path="/companies/:company">
+        <CompanyDetails />
+      </PrivateRoute>
+      <PrivateRoute path="/jobs">
+        <JobList />
+      </PrivateRoute>
+      <PrivateRoute path="/users/:username">
+        <ProfileForm />
+      </PrivateRoute>
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </div>
+  );
+}
 
 export default Routes;
